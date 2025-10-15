8b708a8f69c5be1c0451684de9dda335bbb6bf1c
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-15 12:44:04 +0530

    commit 6 manage two different state in use effect

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react'
 
 function Counter() {
     const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(0)
 
     useEffect(() => {
         console.log("Mounting....");
-        console.log("updating..." + count);
-        return () => {
-            console.log("clean up" + count);
-        }
-
-    }, [count])
+        console.log("updating count1 ..." + count);
+        console.log("updating count2 ..." + count2);
+    }, [count, count2])
 
     return (
         <div>
             <button onClick={() => setCount(count + 1)} >Increment</button>
             <h1> Hello Iam a Component : {count} </h1>
+            <button onClick={() => setCount2(count2 + 1)} >Increment</button>
+            <h1> Hello Iam a Component : {count2} </h1>
         </div>
     )
 }
