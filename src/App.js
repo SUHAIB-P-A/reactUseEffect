728106863c5ccf3ecbf3f9bbb2323a8785af3891
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-15 11:46:00 +0530

    commit 2 new way to writte the code

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <div >
       <h1 onClick={() => setCount(!count)} >Show / Hide</h1>
-      {count ? <Counter /> : null}
+      {count && <Counter />}
     </div>
   );
 }
