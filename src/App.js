7a72f5ad390f57eb348fe2990371c4363b2dd72e
thegreatcode1 <suhaibpa72@gmail.com>
2025-10-15 11:45:03 +0530

    commit 2 show hide in the button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 
 import './App.css';
+import Counter from './components/Counter';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [count, setCount] = useState(false)
+
   return (
-    <div > 
-      <h1>Hello World!</h1>
+    <div >
+      <h1 onClick={() => setCount(!count)} >Show / Hide</h1>
+      {count ? <Counter /> : null}
     </div>
   );
 }
 
 export default App;
 
- // importent terms
+// importent terms
 
- // MOUNTING - load a new component
- // UPDATING - update a component
- // UNMOUNTING - remove a component
+// MOUNTING - load a new component
+// UPDATING - update a component
+// UNMOUNTING - remove a component
